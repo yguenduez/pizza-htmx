425543a0db19e615974994ca22995d1337389d9d
yguenduez <yasinguenduez1@gmail.com>
2024-11-19 20:44:46 +0100

    basic setup with base (inheriting) and including other templates

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,5 +3,8 @@ use axum::response::IntoResponse;
 use crate::templates::HelloTemplate;
 
 pub async fn handler() -> impl IntoResponse {
-    HelloTemplate { name: "Hans" }
+    HelloTemplate {
+        name: "Hans",
+        current_list: vec![1, 33, 44, 88],
+    }
 }
--- src/templates.rs
+++ src/templates.rs
@@ -1,9 +1,15 @@
 use askama::Template;
 
 #[derive(Template)] // this will generate the code...
-#[template(path = "index.html")] // using the template in this path, relative
+#[template(path = "todo_list.html")] // using the template in this path, relative
 pub struct HelloTemplate<'a> {
     // the name of the struct can be anything
     pub name: &'a str, // the field name should match the variable name
-                       // in your template
+    // in your template
+    pub current_list: Vec<i32>,
+}
+
+pub struct TodoItem<'a> {
+    pub name: &'a str,
+    pub due_date: &'a str,
 }
